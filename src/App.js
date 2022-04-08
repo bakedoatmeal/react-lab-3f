5cbed9f20eb0d7b60376b42fe1f5596da029e108
Jane <jane.anqi.cui@gmail.com>
2022-04-08 19:58:34 -0400

    add all elements, controlled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [name, setName] = useState('');
+  const [pepperoni, setPepperoni] = useState(false);
+  const [sausage, setSausage] = useState(false);
+  const [onions, setOnions] = useState(false);
+  const [peppers, setPeppers] = useState(false);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Order'>
+
+        <h1>Pizza Order</h1>
+        <form>
+          <label>Your name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)}></input>
+
+          <label>
+          <input type="checkbox"
+            checked={pepperoni}
+            onChange={() => setPepperoni(!pepperoni)}
+          />
+            Pepperoni
+          </label>
+
+          <label>
+          <input type="checkbox"
+            checked={sausage}
+            onChange={() => setSausage(!sausage)}
+          />
+            Sausage
+          </label>
+
+          <label>
+          <input type="checkbox"
+            checked={peppers}
+            onChange={() => setPeppers(!peppers)}
+          />
+            Peppers
+          </label>
+
+          <label>
+          <input type="checkbox"
+            checked={onions}
+            onChange={() => setOnions(!onions)}
+          />
+            Onions
+          </label>
+
+          <input type="submit"/>
+        </form>
+
+      </div>
+      <div className='display'>
+        <h2>Your order</h2>
+        <p>{name}</p>
+        {pepperoni && <p>Pepperoni</p>}
+        {sausage && <p>Sausage</p>}
+        {peppers && <p>Peppers</p>}
+        {onions && <p>Onions</p>}
+      </div>
     </div>
   );
 }
